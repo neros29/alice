533bd49dd7cbe08c7140dce23c7fb5e2bd38c9f5
neros29 <sorenmile29@gmail.com>
2026-04-10 21:05:47 -0600

    feat: Execute commands in new session

--- bin/relitive_commands.py
+++ bin/relitive_commands.py
@@ -1,12 +1,28 @@
 import subprocess
 import json
 
-raw = subprocess.getoutput("hyprctl -j activeworkspace")
+
+def start_process(command):
+    use_shell = isinstance(command, str)
+    subprocess.Popen(
+        command,
+        shell=use_shell,
+        stdout=subprocess.DEVNULL,  # Redirect output to prevent pipe breaking
+        stderr=subprocess.DEVNULL,
+        stdin=subprocess.DEVNULL,
+        start_new_session=True,      # This is the "disown" magic
+        close_fds=True
+    )
+
+raw = subprocess.getoutput(["hyprctl", "-j", "activeworkspace"])
 workspace_id = str(json.loads(raw)["id"]) 
+
 map_path = "/home/neros/.alice/data/map.json"
+
 with open(map_path, "r") as f:
    map_commands = json.load(f)["map"]
+
 try:
-   subprocess.getoutput(map_commands[workspace_id])
+   start_process(map_commands[workspace_id])
 except KeyError:
-   subprocess.getoutput(map_commands["defualt"])
+   start_process(map_commands["default"])
