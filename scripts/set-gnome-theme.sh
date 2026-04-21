4e3619f50829ca85f0aa73a7b23f5b8967140e67
neros29 <sorenmile29@gmail.com>
2026-04-21 03:44:41 -0600

    refactor(scripts/set-gnome-theme.sh): Update theme switching logic based on file mode

--- scripts/set-gnome-theme.sh
+++ scripts/set-gnome-theme.sh
@@ -4,14 +4,13 @@
 FILE_PATH="$HOME/.config/omarchy/current/theme.json"
 
 # Check if the file exists and if the 'mode' key inside it is 'dark'
-if [[ -f "$FILE_PATH" ]] && jq -r '.mode' "$FILE_PATH" == "dark"; then
+if [[ -f "$FILE_PATH" ]] && [[ $(jq -r '.mode' "$FILE_PATH") == "light" ]]; then
     # Condition is TRUE (File exists AND mode is dark)
-    gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-    gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-else
-    # Condition is FALSE (File does not exist OR mode is not dark)
     gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita"
+else
+    gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+    gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 fi
 
 # Change gnome icon theme color
