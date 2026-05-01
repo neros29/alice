eb21892ced7f1c8d1abd56d3af0778c9957ba3bd
neros29 <sorenmile29@gmail.com>
2026-04-30 19:20:59 -0600

    feat(theme): Implement theme detection and background setting

--- scripts/set-theme.py
+++ scripts/set-theme.py
@@ -7,13 +7,23 @@ import json
 class Main: 
     def __init__(self, root_dir) -> None:
         self.root_dir = os.path.expanduser(root_dir)
+        self.dark = os.path.isfile(os.path.join(self.root_dir, "theme/current/dark"))
+        self.theme_path = os.path.join(self.root_dir, "theme/current/theme.json")
+        self._genrate_theme()
         self.theme = self._get_theme()
         self.theme_mode = self.theme["mode"]
     
     def _get_theme(self):
-        path = os.path.join(self.root_dir, "theme/current/theme.json")
-        with open(path, "r") as f:
+        with open(self.theme_path, "r") as f:
             return json.load(f)
+
+    def _genrate_theme(self):
+        background = os.path.join(self.root_dir, "theme/current/background")
+        mode = "dark" if self.dark else "light"
+        cmd = ["matugen", "image", "-m", mode, background, "--json", "hex", "--source-color-index", "0"]
+        with open(self.theme_path, "w") as f:
+            run(cmd, stdout=f)
+
     def _get_color(self, color: str):
         return self.theme["colors"][color][self.theme_mode]["color"]
 
@@ -50,6 +60,7 @@ background               {self._get_color("background")}
         self.walker_theme()
         self.kitty_theme()
 
+    
 
 if __name__ == "__main__":
     main = Main("~/.alice/")
